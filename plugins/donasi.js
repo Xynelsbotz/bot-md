7ef6f9ac3ce44d3bc5ec5ef35275eb47070f784e
Xynels Design <107124409+Xynelsbotz@users.noreply.github.com>
2022-08-10 05:22:08 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895336282144]
-│ • Gopay  [0895336282144]
-│ • Dana  [0895336282144]
-│ • Saweria  [https://saweria.co/thesadboy01]
+│ • Indosat [085854337533]
+│ • Gopay  [085854337533]
+│ • Dana  [085646458928]
+│ • Saweria  [https://saweria.co/SayuOgiwaraa4]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895336282144
+│ > Ingin donasi? Wa.me/6285854337533
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
